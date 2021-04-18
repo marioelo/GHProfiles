4fb48dce80e84f196df20f42a044522dfb9da8e4
Mario Elorza <mario.elorza@gmail.com>
2021-04-17 21:40:06 -0400

    initial React Native template

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+const App = props => {
+    return (
+      <View style={{justifyContent: 'center', alignItems: 'center', flex: 1}}>
+        <Text>GHProfiles</Text>
+      </View>
+    );
+}
+
+export default App;
